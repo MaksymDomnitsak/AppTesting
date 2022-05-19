30f2ba6070e4b9c332bf2c2927e99fa2389b8ed7
maximuse.domn <user.maximuse@gmail.com>
2022-05-19 19:02:48 +0300

    Changed methods in SuiteTests

--- Selenium/UnitTest1.cs
+++ Selenium/UnitTest1.cs
@@ -1,10 +1,11 @@
-using OpenQA.Selenium;
+п»їusing OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support;
 using System;
 using System.Collections.Generic;
 using Xunit;
-//asserts на перевірку групи,вчителя,h1
+//asserts РЅР° РїРµСЂРµРІС–СЂРєСѓ РіСЂСѓРїРё,РІС‡РёС‚РµР»СЏ,header
 namespace TestProject
 {
     public class SuiteTests : IDisposable
@@ -29,24 +30,15 @@ namespace TestProject
             driver.Manage().Window.Size = new System.Drawing.Size(1920, 1040);
             driver.Manage().Timeouts().ImplicitWait = new TimeSpan(0, 0, 1);
             driver.FindElement(By.Id("group")).Click();
-            {
-                var dropdown = driver.FindElement(By.Id("group"));
-                dropdown.FindElement(By.XPath("//option[. = '32 (302)']")).Click();
-            }
-            {
-                var element = driver.FindElement(By.CssSelector(".svg-btn"));
-                Actions builder = new Actions(driver);
-                builder.MoveToElement(element).Perform();
-            }
-            driver.FindElement(By.CssSelector(".svg-btn")).Click();
-            {
-                var element = driver.FindElement(By.TagName("body"));
-                Actions builder = new Actions(driver);
-                builder.MoveToElement(element, 0, 0).Perform();
-            }
-            vars["2 (01/02/2022-19/05/2022) : 32 (302) Download PDF"] = driver.FindElement(By.CssSelector("h1")).Text;
+
+            var dropdown = driver.FindElement(By.Id("group"));
+            dropdown.FindElement(By.XPath("//option[. = '32 (302)']")).Click();
+
+            var button = driver.FindElement(By.CssSelector(".svg-btn"));
+            Actions builder = new Actions(driver);
+            builder.MoveToElement(button).Perform();
+            button.Click();
             Assert.Equal(driver.FindElement(By.CssSelector("h1")).Text, "Download PDF");
-            //Assert.True(driver.FindElement(By.CssSelector("h1")).Text.Contains("32 (302)"));
         }
 
         [Fact]
@@ -55,24 +47,20 @@ namespace TestProject
             driver.Navigate().GoToUrl("http://develop-softserve.herokuapp.com/");
             driver.Manage().Window.Size = new System.Drawing.Size(844, 725);
             driver.Manage().Timeouts().ImplicitWait = new TimeSpan(0, 0, 1);
-            {
-                var element = driver.FindElement(By.LinkText("Login"));
-                Actions builder = new Actions(driver);
-                builder.MoveToElement(element).Perform();
-            }
+            var login = driver.FindElement(By.LinkText("Login"));
+            Actions builder = new Actions(driver);
+            builder.MoveToElement(login).Perform();
             driver.FindElement(By.Id("group")).Click();
-            {
-                var dropdown = driver.FindElement(By.Id("group"));
-                dropdown.FindElement(By.XPath("//option[. = '32 (302)']")).Click();
-            }
+
+            var dropdown = driver.FindElement(By.Id("group"));
+            dropdown.FindElement(By.XPath("//option[. = '32 (302)']")).Click();
             driver.FindElement(By.CssSelector(".MuiButtonBase-root:nth-child(4) > .MuiButton-label")).Click();
-            {
-                var element = driver.FindElement(By.Id("group"));
-                String value = element.GetAttribute("value");
-                String locator = $"option[@value='{value}']";
-                String selectedText = element.FindElement(By.XPath(locator)).Text;
-                Assert.Equal(selectedText, "32 (302)");
-            }
+
+            var group = driver.FindElement(By.Id("group"));
+            String value = group.GetAttribute("value");
+            String locator = $"option[@value='{value}']";
+            String selectedText = group.FindElement(By.XPath(locator)).Text;
+            Assert.Equal(selectedText, "32 (302)");
         }
 
         [Fact]
@@ -82,15 +70,17 @@ namespace TestProject
             driver.Manage().Window.Size = new System.Drawing.Size(1936, 1056);
             driver.Manage().Timeouts().ImplicitWait = new TimeSpan(0, 0, 2);
             driver.FindElement(By.Id("teacher")).Click();
-                var dropdown = driver.FindElement(By.Id("teacher"));
-                dropdown.FindElement(By.XPath("//option[. = 'Романенко Наталія Вікторівна']")).Click();
+
+            var dropdown = driver.FindElement(By.Id("teacher"));
+            dropdown.FindElement(By.XPath("//option[. = 'Р РѕРјР°РЅРµРЅРєРѕ РќР°С‚Р°Р»С–СЏ Р’С–РєС‚РѕСЂС–РІРЅР°']")).Click();
             driver.FindElement(By.CssSelector(".MuiButtonBase-root:nth-child(4) > .MuiButton-label")).Click();
-            driver.FindElement(By.Id("teacher")).Click();
-                var element = driver.FindElement(By.Id("teacher"));
-                String value = element.GetAttribute("value");
-                String locator = $"option[@value='{value}']";
-                String selectedText = element.FindElement(By.XPath(locator)).Text;
-                Assert.Equal(selectedText, "Романенко Наталія Вікторівна");
+            dropdown.Click();
+
+            var element = driver.FindElement(By.Id("teacher"));
+            String value = element.GetAttribute("value");
+            String locator = $"option[@value='{value}']";
+            String selectedText = element.FindElement(By.XPath(locator)).Text;
+            Assert.Equal(selectedText, "Р РѕРјР°РЅРµРЅРєРѕ РќР°С‚Р°Р»С–СЏ Р’С–РєС‚РѕСЂС–РІРЅР°");
         }
     }
 }
